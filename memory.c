f0f4626f91c529b572692dccc00ad1379d518d73
agaiw <agnieszka.iwaszkiewicz@tieto.com>
2017-02-01 07:47:47 +0100

    Changes after review.

--- memory.c
+++ memory.c
@@ -6,8 +6,6 @@
 #define CHAR_UPPER 126
 
 void memory_print(void* ptr, size_t size);
-void print_row_hex(void* row_ptr, int row_size);
-void print_row_char(void* row_ptr, int row_size);
 
 void main()
 {
@@ -26,13 +24,39 @@ void memory_print(void* ptr, size_t buffer_size)
 
   while (buffer_size > 0)
   {
-
     int row_size = (buffer_size > ROW_LIMIT ? ROW_LIMIT : buffer_size);
     void* row_ptr = ptr + current_row * ROW_LIMIT;
 
-    printf("\n%04x  ", (int)row_ptr);      // print buffer row address
-    print_row_hex(row_ptr, row_size);      // print buffer row as hexadecimals
-    print_row_char(row_ptr, row_size);     // print buffer row as chars
+    // print buffer row address
+    printf("\n%p  ", row_ptr);
+
+    // print buffer row as hexadecimals
+    int row_crawler = row_size;
+
+    for (int i = 0; i < ROW_LIMIT; i++) {
+      
+      printf("%02hhX ", (row_crawler > 0) ? *((int*)(row_ptr + i)) : 0);
+
+      if (i == 7 || i == 15) {
+        printf(" ");
+      }
+
+     row_crawler--;
+
+    }
+
+    // print buffer row as chars
+    for (int i = 0; i < row_size; i++) {
+
+      char oneChar = *(char*)(row_ptr + i);
+
+      printf("%c", (oneChar >= CHAR_LOWER && oneChar <= CHAR_UPPER) ? oneChar : '.');
+
+      if (i == 7) {
+        printf(" ");
+      }
+
+    }
 
     buffer_size = (buffer_size > ROW_LIMIT ? buffer_size - ROW_LIMIT : 0);
     current_row++;
@@ -40,35 +64,5 @@ void memory_print(void* ptr, size_t buffer_size)
   }
 
   printf("\n");
-
-}
-
-void print_row_hex(void* row_ptr, int row_size)
-{
-
-  for (int i = 0; i < ROW_LIMIT; i++)
-  {
-
-    (row_size > 0) ? printf("%02hhX ", *((int*)(row_ptr + i))) : printf("00 ");
-    if (i == 7 || i == 15) printf(" ");
-    row_size--;
-
-  }
-
-}
-
-void print_row_char(void* row_ptr, int row_size)
-{
-
-  for (int i = 0; i < row_size; i++)
-  {
-
-    ( *(char*)(row_ptr + i) >= (char)CHAR_LOWER &&
-      *(char*)(row_ptr + i) <= (char)CHAR_UPPER )
-       ? printf("%c", *(char*)(row_ptr + i))
-       : printf("."); 
-
-    if (i == 7) printf(" ");
-  }
 
 }
