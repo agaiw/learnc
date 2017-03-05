00b5b057c7e571a905151f924270b74578ef4395
agaiw <agnieszka.iwaszkiewicz@tieto.com>
2017-03-05 13:33:32 +0100

    threads tuned

--- bitmap/bitmap.c
+++ bitmap/bitmap.c
@@ -2,26 +2,98 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-  struct parameters {
-    int bitmapHeight;
-    int bitmapWidth;
-    int rowSize;
-    unsigned char* pixelArray_p;
-    int offset;
-    int* hArray;
+struct bitmap {
 
-  } parameters;
+  int bmpHeight;
+  int bmpWidth;
+  int bmpRowSize;
+  unsigned char* pixelArray_p;
 
-void fillHistogram(struct parameters* parameters);
+} bitmap;
 
-int main() {
+struct histogram {
+
+  int offset;
+  int* hArray;
+  struct bitmap* bitmap;
+
+} histogram;
+
+char* loadFile(char* filename);
+void readBitmapProperties(char* buffer, struct bitmap* bitmap);
+void fillHistogram(struct histogram* histogram);
+
+int main(int argc, char* argv[]) {
+
+  char* buffer = loadFile(argv[1]);
+  readBitmapProperties(buffer, &bitmap);
+  
+  int hRed[256];
+  int hGreen[256];
+  int hBlue[256]; 
+
+  struct histogram histogramB = {2, hBlue, &bitmap};
+  struct histogram histogramG = {1, hGreen, &bitmap};
+  struct histogram histogramR = {0, hRed, &bitmap};
+
+  //threads
+  pthread_t threadR;
+  pthread_t threadG;
+  pthread_t threadB;
+
+  pthread_create(&threadB, NULL, fillHistogram, &histogramB);
+  pthread_create(&threadG, NULL, fillHistogram, &histogramG);
+  pthread_create(&threadR, NULL, fillHistogram, &histogramR);
+
+  pthread_join(threadB, NULL);
+  pthread_join(threadG, NULL);
+  pthread_join(threadR, NULL);
+
+  free(buffer);
+
+  for (int i = 0; i < 256; i++) {
+
+    printf("%d\t%d\t%d\t%d\t\n", i, hBlue[i], hGreen[i], hRed[i]);
+
+  }
+
+  return 0;
+
+}
+void fillHistogram(struct histogram* histogram) {
+
+  for (int i = 0; i < 256; i++) {
+
+    *(histogram->hArray + i) = 0;
+
+  }
+  unsigned char* tempArray_p = (histogram->bitmap)->pixelArray_p;
+
+  for (int i = 0; i < (histogram->bitmap)->bmpHeight; i++) {
+
+    unsigned char* tempRow_p = tempArray_p;
+
+    for (int j = 0; j < (histogram->bitmap)->bmpWidth; j++) {
+
+      histogram->hArray[*(tempRow_p + histogram->offset)] += 1;
+      tempRow_p += 3;
+
+      }
+
+    tempArray_p += (histogram->bitmap)->bmpRowSize;
+
+  }
+  return;
+}
+
+char* loadFile(char* filename) {
 
   FILE* pFile;
   long lSize;
   char* buffer;
   size_t result;
 
-  pFile = fopen("sample.bmp", "rb");
+  pFile = fopen(filename, "rb");
   
   fseek(pFile, 0, SEEK_END);
   lSize = ftell(pFile); 
@@ -32,80 +104,16 @@ int main() {
   result = fread(buffer, 1, lSize, pFile);
   fclose(pFile);
 
-  int pixelArrayOffset = *((int*)(buffer + 10));
- 
-  unsigned char* pixelArray_p = buffer + 54;
-
-  int bitmapWidth = *((int*)(buffer + 18));
-  int bitmapHeight = *((int*)(buffer + 22));
-
-  short bitsPerPixel = *((short*)(buffer + 28)); //24
-
-  int rowSize = ((int)((bitsPerPixel * bitmapWidth + 31) / 32)) * 4;
-
-  int hRed[256];
-  int hGreen[256];
-  int hBlue[256]; 
-
-  struct parameters parametersB = {bitmapHeight, bitmapWidth, rowSize, pixelArray_p, 2, hBlue};
-  struct parameters parametersG = {bitmapHeight, bitmapWidth, rowSize, pixelArray_p, 2, hGreen};
-  struct parameters parametersR = {bitmapHeight, bitmapWidth, rowSize, pixelArray_p, 2, hRed};
-
-  //threads
-  pthread_t threadR, threadG, threadB;
-
-  pthread_create(&threadB, NULL, &fillHistogram, &parametersB);
-  pthread_create(&threadG, NULL, &fillHistogram, &parametersG);
-  pthread_create(&threadR, NULL, &fillHistogram, &parametersR);
-
-  pthread_join(threadB, NULL);
-  pthread_join(threadG, NULL);
-  pthread_join(threadR, NULL);
-
-  free(buffer);
-
- 
-  for(int i = 0; i < 256; i++) {
-    printf("%d\t%d\t%d\t%d\n", i, hBlue[i], hGreen[i], hRed[i]); 
-  }
-
-  return 0;
-
-}
-void fillHistogram(struct parameters* parameters) {
-
-  for (int i = 0; i < 256; i++) {
-
-    *(parameters->hArray + i) = 0;
-
-  }
-
-  unsigned char* tempArray_p = parameters->pixelArray_p;
-
-  for (int i = 0; i < parameters->bitmapHeight; i++) {
-
-    unsigned char* tempRow_p = tempArray_p;
-
-    for (int j = 0; j < parameters->bitmapWidth; j++) {
-
-      parameters->hArray[*(tempRow_p + parameters->offset)] += 1;
-      tempRow_p += 3;
-
-    }
-
-    tempArray_p += parameters->rowSize;
-
-  return;
+  return buffer;
 
 }
 
+void readBitmapProperties(char* buffer, struct bitmap* bitmap) {
 
-
-
-
-
-
+  bitmap->pixelArray_p = buffer + 54;
+  bitmap->bmpWidth = *((int*)(buffer + 18));
+  bitmap->bmpHeight = *((int*)(buffer + 22));
+  short bitsPerPixel = *((short*)(buffer + 28));
+  bitmap->bmpRowSize = ((int)((bitsPerPixel * bitmap->bmpWidth + 31) / 32)) * 4;
 
 }
-
-
